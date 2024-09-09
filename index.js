6a2e36d501d1b7af6e4109657c8953c07c9e26d2
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2024-09-09 22:05:58 +0200

    cors was updated

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 26000;
 
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'], 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true, 
+}));
 
 mongooseClient();
 
